0e4578471d572d15d0d0acd1c899ba634d4f2519
Artur Sentsov <sen4ik@gmail.com>
2026-04-27 19:43:09 -0700

    feat: add createLookup factory

---
+++ src/lookup.ts
@@ -0,0 +1,17 @@
+import type { NcicLookup, NcicMapPair } from './types.js';
+
+export function createLookup(pair: NcicMapPair): NcicLookup {
+  return {
+    getName(code: string): string | undefined {
+      return pair.forward[code.toUpperCase()];
+    },
+
+    getCode(name: string): string | undefined {
+      return pair.reverse[name.toUpperCase()];
+    },
+
+    all(): Record<string, string> {
+      return pair.forward;
+    },
+  };
+}
